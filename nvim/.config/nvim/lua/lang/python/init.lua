8e5b80570f35972509cac4e59e5ca86ae99b4e60
Ellison Leão <ellisonleao@gmail.com>
2020-03-23 11:58:09 -0300

    adding test runner for python

--- nvim/.config/nvim/lua/lang/python/init.lua
+++ nvim/.config/nvim/lua/lang/python/init.lua
@@ -1,5 +1,10 @@
 --- Python layer
+local autocmd = require("cfg.autocmd")
 local layer = {}
+
+local function on_filetype_python()
+  vim.g["test#python#runner"] = "pytest"
+end
 
 --- Returns plugins required for this layer
 function layer.register_plugins()
@@ -11,6 +16,7 @@ function layer.init_config()
   local nvim_lsp = require("nvim_lsp")
 
   lsp.register_server(nvim_lsp.pyls)
+  autocmd.bind_filetype("python", on_filetype_python)
 end
 
 return layer
