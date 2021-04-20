0c8c965175dd9036065a1a05ecbf123877230af0
Ellison Leão <ellisonleao@gmail.com>
2021-04-20 10:28:11 -0300

    fix mapping

--- configs/.config/nvim/lua/editor.lua
+++ configs/.config/nvim/lua/editor.lua
@@ -179,7 +179,7 @@ local mappings = {
   {"n", "<leader>q", [[<Cmd>bp<CR>]], opts}, -- move to the previous buffer (same option, different key)
   {"n", "<leader>x", [[<Cmd>bn<CR>]], opts}, -- move to the next buffer
   {"n", "<leader>w", [[<Cmd>bn<CR>]], opts}, -- move to the next buffer (same option, different key)
-  {"n", "<leader>d", [[<C-W><C-Q>]], opts}, -- close current buffer
+  {"n", "<leader>d", [[<Cmd>bd<CR>]], opts}, -- close current buffer
   {"n", "<leader>c", [[<Cmd>cclose<CR>]], opts}, -- close quickfix list
   {"n", "<leader>h", [[<Cmd>split<CR>]], opts}, -- create horizontal split
   {"n", "<leader>v", [[<Cmd>vsplit<CR>]], opts}, -- create vertical split
